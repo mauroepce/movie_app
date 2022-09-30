f60e0027b134d49745dd9c40d18c0974e0ad9f0d
mauroepce <mauricioeslainerpc@gmail.com>
2022-09-30 15:18:07 -0300

    Create Movie

--- src/components/CreateMovie/CreateMovie.jsx
+++ src/components/CreateMovie/CreateMovie.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import * as actions from "./../../redux/actions/index"
 // Importar las actions como Object Modules, sino los test no funcionarán!
 
 // Fijense en los test que SI O SI tiene que ser un functional component, de otra forma NO VAN A PASAR LOS TEST
@@ -8,10 +10,50 @@ import React from "react";
 
 const CreateMovie = () => {
 
+  let dispatch = useDispatch();
+
+  const [input, setInput] = React.useState({
+    name: "",
+    releaseYear: 0,
+    description: "",
+    director: "",
+  })
+
+  const handleInputChange = e => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(actions.createMovie(input))
+  }
 
   return (
     <div>
-      
+      <form action="" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="">Name: </label>
+          <input type="text" name="name" value={input.name} onChange={handleInputChange} />
+        </div>
+        <div>
+          <label htmlFor="">ReleaseYear: </label>
+          <input type="number" name="releaseYear" value={input.releaseYear} onChange={handleInputChange}/>
+        </div>
+        <div>
+          <label htmlFor="">Description: </label>
+          <textarea name="description" id="" cols="30" rows="10" value={input.description} onChange={handleInputChange}/>
+        </div>
+        <div>
+          <label htmlFor="">Director: </label>
+          <input type="text" name="director" value={input.director} onChange={handleInputChange}/>
+        </div>
+        <div>
+          <button type="submit">Create Movie</button>
+        </div>
+      </form>
     </div>
   );
 };
