04bdb112d6c2f399caf88eca0234c17152dcef70
mauroepce <mauricioeslainerpc@gmail.com>
2022-09-30 12:42:32 -0300

    02.test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Home from "./components/Home/Home";
+import Nav from "./components/Nav/Nav";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import CreateMovie from "./components/CreateMovie/CreateMovie";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      Test
-    </div>
+    <React.Fragment>
+      <Nav />
+      <Route exact path ="/" component={Home} />
+      <Route path="/movie/:id" componente={MovieDetail} />
+      <Route exact path="/movies/create" component={CreateMovie} />
+    </React.Fragment>
+    
   );
 }
 export default App;
