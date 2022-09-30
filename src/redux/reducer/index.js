5f331f73497b939ac3c0eb0ff7ba80bad0c6a2d4
mauroepce <mauricioeslainerpc@gmail.com>
2022-09-30 13:45:58 -0300

    reducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,4 +1,5 @@
 // Importa las action types acá
+import {GET_ALL_MOVIES, GET_MOVIE_DETAILS, CREATE_MOVIE, DELETE_MOVIE, SEND_EMAIL} from "../actions"
 
 
 const initialState = {
@@ -10,7 +11,23 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     // Acá va tu código:
+    case GET_ALL_MOVIES:
+      return {...state, movies: action.payload}
 
+    case GET_MOVIE_DETAILS:
+      return {...state, movieDetail: action.payload}
+
+    case CREATE_MOVIE:
+      return {...state, movies: [...state.movies, action.payload]}
+
+    case DELETE_MOVIE:
+      return {...state, movies: state.movies.filter(movie => movie.id !== action.payload)}
+
+    case SEND_EMAIL:
+      return {...state, email: action.payload}
+    
+    default:
+      return state
   
   }
 };
